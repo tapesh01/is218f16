463a612392cb24cccc414c514fd2c0401a7dec56
Tapesh Nagarwal <tn64@afsconnect2.njit.edu>
2016-10-18 11:41:26 -0400

    added registration page

---
+++ registration.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<head>
+		<title> Welcome to the IS Community Sign up Page!</title>
+	</head>
+		<body>	
+			<center>
+				<h3>Tapesh Nagarwal's Sign Up Page:</h3>
+				<p> Please Accurately Fill out the Form:</p>
+				<form>	
+					First Name: <br>
+					<input type="text" name="firstname"><br>
+					Last Name: <br>
+					<input type="text" name="lastname"><br>
+					UserName: <br>
+					<input type="text" name="username"><br>
+					Password: <br>
+					<input type="password" name="password"><br>
+					Confirm Password:<br>
+					<input type="password" name="cnfrmpswd"><br>
+					<input type="submit" name="Submit"></br>
+		</center>
+		
+	</body>
+</html>
+
