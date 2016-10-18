53c737394748445678c72a6302a70ede337560ef
Tapesh Nagarwal <tn64@afsconnect2.njit.edu>
2016-10-18 11:41:58 -0400

    added sign in page' :

---
+++ signin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<body>
+		<center>
+			<div>
+			<form>
+				Username: <br>
+				<input type="text" name="username"><br>
+				Password: <br>
+				<input type="password" name="pswd"><br>
+				<input type="submit" name="Submit"></br>
+			</form>
+			</div>
+		</center>
+		</body>
+	</head>
+</html>
